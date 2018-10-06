60a36e5f5a126873b780493f84c4117a452a9e4e
venu2804 <venu2804@gmail.com>
2018-10-06 19:01:26 +0100

    First Commit

---
+++ src/academy/learnProgramming/ImportExample.java
@@ -0,0 +1,16 @@
+package academy.learnProgramming;
+
+
+import a.b.c.d.MyClass;
+
+import java.util.Random;
+
+public class ImportExample {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt(10));
+
+        MyClass
+    }
+}
